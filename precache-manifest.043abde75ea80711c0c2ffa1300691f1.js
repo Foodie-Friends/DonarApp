688ecc7b96f1d55fc068e7c92c06e9a874372c98
Tushar <s.v.tushar04@gmail.com>
2023-03-21 08:40:20 +0530

    Updates

---
+++ precache-manifest.043abde75ea80711c0c2ffa1300691f1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b844d37b78a1f741f80",
+    "url": "/DonarApp/static/css/main.8090ac2a.chunk.css"
+  },
+  {
+    "revision": "2b844d37b78a1f741f80",
+    "url": "/DonarApp/static/js/main.2b844d37.chunk.js"
+  },
+  {
+    "revision": "692a8372bce424561495",
+    "url": "/DonarApp/static/js/1.692a8372.chunk.js"
+  },
+  {
+    "revision": "0196815c98a0444fbec2",
+    "url": "/DonarApp/static/js/2.0196815c.chunk.js"
+  },
+  {
+    "revision": "7bd0d46e3e53aac280b6",
+    "url": "/DonarApp/static/js/runtime~main.7bd0d46e.js"
+  },
+  {
+    "revision": "916f8e5a685f54a15d44fe0b89607f7c",
+    "url": "/DonarApp/static/media/dish1.916f8e5a.png"
+  },
+  {
+    "revision": "a268ee4bc7866a255e644a7ea9f2d2d8",
+    "url": "/DonarApp/static/media/logo.a268ee4b.png"
+  },
+  {
+    "revision": "656acad24e876b1cc528f55093a4ad82",
+    "url": "/DonarApp/static/media/wave.656acad2.png"
+  },
+  {
+    "revision": "1bc720c63d0260b0361a22f02b73b8fa",
+    "url": "/DonarApp/static/media/grey.1bc720c6.jpg"
+  },
+  {
+    "revision": "a6612b008a57eb7c5829013a1fa59522",
+    "url": "/DonarApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/DonarApp/Login/precache-manifest.e67fed92ccabeb0ca0ab20525c2a2056.js"
+  "/DonarApp/precache-manifest.043abde75ea80711c0c2ffa1300691f1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/DonarApp/Login/index.html", {
+workbox.routing.registerNavigationRoute("/DonarApp/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
